46a218476b7e8381faa01839e2f3d11721350fbb
Greg Sutcliffe <github@emeraldreverie.org>
2018-09-04 13:23:54 +0100

    Switch to Tab layout and add Plotly graphs

--- ui.R
+++ ui.R
@@ -8,34 +8,49 @@
 #
 
 library(shiny)
+library(plotly)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
 
   # Application title
-  titlePanel("Foreman Redmine"),
+  titlePanel("Foreman Community Stats"),
 
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-      h5(paste('Last Updated:',file.info('/tmp/issues.csv')$ctime),
-               style = "text-decoration: underline"),
-      selectInput("project", "Project:",
-                  c("Foreman"),c("Foreman")),
-      checkboxGroupInput('all_none','Select All/None',c('All','None')),
-      checkboxGroupInput('cats', 'Categories')
+  tabsetPanel(
+    tabPanel("Category Issues",
+      sidebarLayout(
+        sidebarPanel(
+          h5(paste('Last Updated:',file.info('/tmp/issues.csv')$ctime),
+            style = "text-decoration: underline"),
+          selectInput("project", "Project:",
+            c("Foreman"),c("Foreman")),
+          checkboxGroupInput('all_none','Select All/None',c('All','None')),
+          checkboxGroupInput('cats', 'Categories')
+        ),
+
+        mainPanel(
+          uiOutput('project'),
+          uiOutput('header'),
+          plotOutput("categories"),
+          p('Shows the number of open bugs per category for the selected project.
+            The proportion of bugs flagged as "Triaged" is shown in colour.'),
+          p('Use the slect box on the left to pick a new project, or the checkboxes
+            to limit which categories are shown.')
+        )
+      )
     ),
-
-    # Show a plot of the generated distribution
-    mainPanel(
-      uiOutput('project'),
-      uiOutput('header'),
-      plotOutput("categories"),
-      p('Shows the number of open bugs per category for the selected project.
-        The proportion of bugs flagged as "Triaged" is shown in colour.'),
-      p('Use the slect box on the left to pick a new project, or the checkboxes
-        to limit which categories are shown.'),
-      dygraphOutput('open_closed')
+    tabPanel("Open/Closed",
+      plotlyOutput('open_closed'),
+      p('Open & Closed bugs over all time. TODO - filter by project / category :)')
+    ),
+    tabPanel("Users",
+      plotlyOutput('users'),
+      p('Shows the "age" of accounts which have interacted with Redmine in the last 6 months.'),
+      p('Here, "interacted" means logged/changed a ticket, or added a text comment'),
+      hr(),
+      plotlyOutput('users_box'),
+      p('Same data as a box-plot, where it`s easier to see median and range information')
     )
   )
+
 ))
